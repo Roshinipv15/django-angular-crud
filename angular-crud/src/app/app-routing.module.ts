02acf1c580e16a3bc6a51d091a124505d8abd5c5
Roshinipv15 <91727558+Roshinipv15@users.noreply.github.com>
2022-07-27 16:49:48 +0530

    changes

--- angular-crud/src/app/app-routing.module.ts
+++ angular-crud/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './crud/register/register.component';
 
 const routes: Routes = [
-  { path: '', component: RegisterComponent }
+  { path: 'register', component: RegisterComponent }
 ];
 
 @NgModule({
